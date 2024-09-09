f4c060d73e26550ff944f1f0de07b56056fda09a
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-09-08 20:01:10 -0500

    sd

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,17 +4,16 @@ let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, comma
   throw false
   }
   let pesan = args.join` `
-  let oi = `*𝐌𝐞𝐧𝐬𝐚𝐣𝐞:* ${pesan}`
-  let teks = `𝐕𝐞𝐧𝐭𝐚𝐬 𝐝𝐞 𝐛𝐨𝐭𝐬,𝐚𝐫𝐜𝐡𝐢𝐯𝐨𝐬,𝐬𝐞𝐧𝐬𝐢𝐬,𝐜𝐮𝐞𝐧𝐭𝐚𝐬 wa.me/51992621601
-  *⺀EN MIS TIEMPOS YO TE HUBIERA COMPRADO ATI 🙆🏿‍♂️⺀*\n\n🌩️ ${oi}\n\n⚽ *𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚:*\n`
+  let oi = `*‼️ CUCU INVOCANDO MOCHOS ‼️:* ${pesan}`
+  let teks = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝚀𝚄𝙴 𝙴𝚂𝙿𝙴𝚁𝙰𝙽 𝙿𝙰𝚁𝙰 𝙰𝙽𝙾𝚃𝙰𝚁𝚂𝙴 𝚄𝙽𝙰 𝚃𝙰𝚁𝙹𝙴𝚃𝙰 𝙳𝙴 𝙸𝙽𝚅𝙸𝚃𝙰𝙲𝙸𝙾́𝙽 𝙾 𝚀𝚄𝙴 𝙼𝙼𝚅𝙶*\n\n🟩 ${oi}\n\n🟩 *@𝕒𝕝𝕖.𝕚𝕫𝕟_𝟚𝟘:*\n`
   for (let mem of participants) {
-  teks += `▌🎠 @${mem.id.split('@')[0]}\n`}
-  teks += `◥ 𝐁𝐥𝐮𝐞 𝐋𝐨𝐜𝐤 ◤`
+  teks += `┊🧧 @${mem.id.split('@')[0]}\n`}
+  teks += `‼️ 𝐂𝐔𝐂𝐔 𝐁𝐎𝐓 ‼️`
   conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
   }
   handler.help = ['tagall <mesaje>','invocar <mesaje>']
   handler.tags = ['group']
-  handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+  handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten)$/i
   handler.admin = true
   handler.group = true
   export default handler
